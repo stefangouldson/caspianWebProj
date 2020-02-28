ce3045742ee897b473bb37ecadfda74b706ddfca
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-28 11:08:33 +0000

    port tweaks

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
+const port = process.env.port || 4000
 
 app.use(express.static(path.join(__dirname, "src")))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log('listening to localhost:4000')
 })
